90c4331e3b2a0e5b9f72e9d265b8b351cc1c8308
Caden Hawkins <hawkicad@oregonstate.edu>
2022-04-25 23:26:17 -0700

    Delete src directory

--- src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { exerciseApi } from '../services/exerciseAPI';
-
-export default configureStore({
-    reducer: {
-        [exerciseApi.reducerPath]: exerciseApi.reducer,
-    },
-});
